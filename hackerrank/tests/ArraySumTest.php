17915551cc460ec2284d09903346f180c1cef8a9
Bert Van de Casteele (Alienpruts) <vandecasteele@evilones.be>
2018-11-17 14:38:01 +0100

    Modiefied HackerRank ArraySumTest to test for very large integers

--- hackerrank/tests/ArraySumTest.php
+++ hackerrank/tests/ArraySumTest.php
@@ -18,8 +18,12 @@ class ArraySumTest extends TestCase
 
         $array = [1, 2, -4, 10, -5];
         $array2 = [-10, -100, 50, 25];
+        $array3 = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005];
+
         $this->assertEquals(4, $arraySum->arraySum($array));
         $this->assertEquals(-35, $arraySum->arraySum($array2));
+        $this->assertEquals(5000000015, $arraySum->arraySum($array3));
+
 
     }
 }
